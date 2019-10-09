9335a5c5a083ee5002985782aae587207374e041
Bretton B Holmes <famunkwriter@me.com>
2019-10-09 12:38:52 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   counter = 0
 
   while counter < array.length do
-    array << new_array[counter].square_array
+    array << array[counter].square_array
     counter += 1
   end
   new_array
